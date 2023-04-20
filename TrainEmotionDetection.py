e89f94e98650985705f7419b9c8bd0c163e6bf8b
MONSOON MAURYA <itssinghtite@gmail.com>
2023-04-20 13:57:26 +0530

    first commit

---
+++ TrainEmotionDetection.py
@@ -0,0 +1,84 @@
+# import required packages
+import cv2
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+# Initialize image data generator with rescaling
+train_data_gen = ImageDataGenerator(rescale=1./255)
+validation_data_gen = ImageDataGenerator(rescale=1./255)
+
+# Preprocess all test images
+train_generator = train_data_gen.flow_from_directory(
+        'data/train',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# Preprocess all train images/\
+validation_generator = validation_data_gen.flow_from_directory(
+        'data/test',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# create model structure
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0001, decay=1e-6), metrics=['accuracy'])
+
+# Set a learning rate annealer
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
+                                            patience=3,
+                                            verbose=1,
+                                            factor=0.5,
+                                            min_lr=0.00001)
+
+
+
+
+# Train the neural network/model
+emotion_model_info = emotion_model.fit(
+        train_generator,
+        steps_per_epoch=28709 // 64,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=7178 // 64)
+
+loss, accuracy = emotion_model.evaluate(train_generator, verbose=1)
+loss_v, accuracy_v = emotion_model.evaluate(validation_generator, verbose=1)
+print("Test : accuracy = %f  ;  loss_v = %f" % (accuracy_v, loss_v))
+print("Train : accuracy = %f  ;  loss = %f" % (accuracy, loss))
+
+# save model structure in jason file
+model_json = emotion_model.to_json()
+with open("emotion_model.json", "w") as json_file:
+    json_file.write(model_json)
+
+
+
+# save trained model weight in .h5 file
+emotion_model.save_weights('emotion_model.h5')
